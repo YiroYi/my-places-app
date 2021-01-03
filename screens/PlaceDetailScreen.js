3e49c669d639cd1462bb910668cfcd4a6f4c6683
YiroYi <yiroyi@live.com>
2021-01-02 18:09:34 -0600

    Places navigator file created

---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+const PlaceDetailScreen = props => {
+  return (
+
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default PlaceDetailScreen;
+
---
+++ screens/PlacesListScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+const PlacesListScreen = props => {
+  return (
+
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default PlacesListScreen;
+
